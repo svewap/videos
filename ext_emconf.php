5332b508537684d9d0cf124d9049c9c5b61f314b
Sven Wappler <info@wappler.systems>
2021-02-27 03:26:39 +0100

    dependencies updated

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,35 +1,35 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-	'title' => 'Videos',
-	'description' => 'Video player for playlists with cue points, preview image and subtitles',
-	'author' => 'Sven Wappler',
-	'author_email' => 'typo3@wappler.systems',
-	'category' => 'misc',
-	'author_company' => 'WapplerSystems',
-	'shy' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 1,
-	'lockType' => '',
-	'version' => '0.1.0',
-	'constraints' => [
-		'depends' => [
-			'typo3' => '8.7.1-9.5.99',
+    'title' => 'Videos',
+    'description' => 'Video player for playlists with cue points, preview image and subtitles',
+    'author' => 'Sven Wappler',
+    'author_email' => 'typo3@wappler.systems',
+    'category' => 'misc',
+    'author_company' => 'WapplerSystems',
+    'shy' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 1,
+    'lockType' => '',
+    'version' => '0.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '9.5.0-9.5.99',
             'filemetadata' => ''
         ],
-		'conflicts' => [
+        'conflicts' => [
         ],
-		'suggests' => [
+        'suggests' => [
         ],
     ],
-	'suggests' => [
+    'suggests' => [
     ],
 ];
 
